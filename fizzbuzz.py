282242953bbd0376c92e1ad368308171c8670d8b
coder-Aayush <bhattaraiaayush08@gmail.com>
2021-10-31 22:14:12 +0545

    program implementation for fizzbuzz challenge

---
+++ fizzbuzz.py
@@ -0,0 +1,7 @@
+def checkFizzBuzz():
+    for i in range(1,101):
+        fizz = 'Fizz' if i%3==0 else ''
+        buzz = 'Buzz' if i%5==0 else ''
+        print(f'{fizz}{buzz}' or i)
+
+checkFizzBuzz()
